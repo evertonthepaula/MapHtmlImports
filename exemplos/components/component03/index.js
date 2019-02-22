fd31d6193fafe743a0efa418e7467159bf5ac09d
evertonthepaula <evertothepaula@gmail.com>
2019-02-22 14:14:39 -0300

    Melhorar a organização do projeto adicionando exemplos
    Muda a hierarquia de diretórios;
    Adiciona exemplos;
    melhora a documentação para refletir as mudanças.

---
+++ exemplos/components/component03/index.js
@@ -0,0 +1,17 @@
+class CustomElementClass03 extends HTMLElement
+{
+    constructor()
+    {
+        super();
+        const template = document.currentScript.ownerDocument.querySelector('template');
+        const dataMode = this.dataset.mode;
+        const shadowRoot = this.attachShadow({mode: dataMode});
+        shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+if(!'customElements' in window) {
+    console.error( new Error('Seu browser não suporta a customElements') );
+}
+
+customElements.define('custom-element03', CustomElementClass03);
