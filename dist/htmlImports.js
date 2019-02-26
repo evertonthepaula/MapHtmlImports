f486f5f5fba164d2dc7f654e43804b1829ada9f5
evertonthepaula <evertothepaula@gmail.com>
2019-02-26 13:32:26 -0300

    Corrigir importação de Js modules e implementar exemplos de uso.

--- dist/htmlImports.js
+++ dist/htmlImports.js
@@ -1,4 +1,5 @@
 class htmlImports {
+
     constructor() {
         this.testSupport();
     }
@@ -18,27 +19,27 @@ class htmlImports {
         const tag = this.defineTag(obj);
 
         tag.setAttribute('async', 'async');
-        tag.onload = function (e) { console.log('%c DEU CERTO!!!', 'font-weight: bold; font-size: 40px;color: red; padding-bottom: 24px; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)'); };
-        tag.onerror = function (e) { console.warn('ZICOU!'); };
+        tag.onload = obj.callbackLoaded;
+        tag.onerror = obj.callbackError;
         document.head.appendChild(tag);
-
+        
         return tag;
     }
 
-    defineTag(obj) {
-        if (RegExp('(.html)$')) {
+    defineTag(obj){
+        if(RegExp('(.html)$').test(obj.path)){
             const link = document.createElement('link');
             link.rel = 'import';
             link.href = obj.path;
-
+        
             return link;
         }
 
-        if (RegExp('(.js)$')) {
+        if(RegExp('(.js)$').test(obj.path)){
             const script = document.createElement('script');
             script.type = 'module';
             script.src = obj.path;
-
+            
             return script;
         }
 
